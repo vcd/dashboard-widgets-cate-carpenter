2ca9278d0a911ee47821cbcd1ff1aa6851e02eac
Cate Carpenter <ccarpenter5@ewu.edu>
2022-05-03 11:14:38 -0700

    Edits

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,7 @@
 // API key. Replace with your API key
 const APIKEY = 'f2f85762267ef18d0df71e18ec9f3737';
 // City
-const city = 'Spokane';
+const city = 'Forks';
 // Units for Farenheit
 const units = 'imperial';
 
@@ -21,10 +21,10 @@ fetch(url)
   
   // Template to output
   const template = `
+  <h2>⛅</h2>
     <h1>Weather</h1>
     <data value="${data.name}" class="city">${data.name}</data>
     <data value="${data.main.temp}" class="temp">${data.main.temp}&#8457;</data>
-    <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Placeholder">
   `;
   
   // Insert dynamic template to container
